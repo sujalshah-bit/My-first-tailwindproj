d1c91b83727ce2550f1b252abd2583dd075c7ef5
sujal shah <sujalshah28092004@gmail.com>
2022-12-08 19:59:53 +0530

    Push all files except node module and confSujal

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'fb-bg':'#F0F2F5',
+      },
+      height:{
+        'h-22':'22rem'
+      }
+    },
+  },
+  plugins: [],
+}
